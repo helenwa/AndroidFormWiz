bbd928cfa0920df76787afd96df2f87cf8ae1de4
helenwa <wallach@tcd.ie>
2016-10-24 18:26:02 +0100

    now store image instead of passing.

--- app/src/main/java/com/wallace/happy/androidformwiz/ProcessImageTemplateActivity.java
+++ app/src/main/java/com/wallace/happy/androidformwiz/ProcessImageTemplateActivity.java
@@ -1,21 +1,41 @@
 package com.wallace.happy.androidformwiz;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
-import static android.R.attr.bitmap;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 
 public class ProcessImageTemplateActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_process_image_template);
-        Bitmap bitmap = (Bitmap) getIntent().getParcelableExtra("Image");
-        ImageView imageView = (ImageView) findViewById(R.id.imageView);
-        imageView.setImageBitmap(bitmap);
+        Intent intent = getIntent();
+        String templateReference = intent.getStringExtra(SelectFormTemplateActivity.TEMP_REF);
+        loadImageFromStorageTOScreen(templateReference);
         //todo image processing
+    }
+
+    private void loadImageFromStorageTOScreen(String path)
+    {
+
+        try {
+            File f=new File(path, "newTemplate.jpg");
+            Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
+            ImageView img=(ImageView)findViewById(R.id.imageView);
+            img.setImageBitmap(b);
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+
     }
 }
--- app/src/main/java/com/wallace/happy/androidformwiz/SelectFormTemplateActivity.java
+++ app/src/main/java/com/wallace/happy/androidformwiz/SelectFormTemplateActivity.java
@@ -1,5 +1,7 @@
 package com.wallace.happy.androidformwiz;
 
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -7,14 +9,14 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
-import static android.R.attr.data;
-
 public class SelectFormTemplateActivity extends AppCompatActivity {
+    public final static String TEMP_REF = "com.wallace.happy.androidformwiz.TEMP_REF";
 
     private int PICK_IMAGE_REQUEST = 1;
     @Override
@@ -79,7 +81,9 @@ public class SelectFormTemplateActivity extends AppCompatActivity {
         if(bitmap!=null) {
             Intent intent = new Intent(this, ProcessImageTemplateActivity.class);
             //*      To Pass text/variables to new intent
-            intent.putExtra("Image", bitmap);
+            String imagePath = saveToInternalStorage(bitmap);
+            intent.putExtra(TEMP_REF, imagePath);
+            //intent.putExtra("Image", bitmap);
             startActivity(intent);
         }
         else {
@@ -87,6 +91,29 @@ public class SelectFormTemplateActivity extends AppCompatActivity {
         }
     }
 
+    private String saveToInternalStorage(Bitmap bitmapImage){
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        // path to /data/data/yourapp/app_data/imageDir
+        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        // Create imageDir
+        File mypath=new File(directory,"newTemplate.jpg");
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(mypath);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return directory.getAbsolutePath();
+    }
 
 
 
