a80d4688c636d1bcd330d5513feef520a3872e76
helenwa <wallach@tcd.ie>
2017-02-13 11:56:54 +0000

    fixed all issues with image quality

--- app/src/main/java/com/wallace/happy/androidformwiz/SelectFormTemplateActivity.java
+++ app/src/main/java/com/wallace/happy/androidformwiz/SelectFormTemplateActivity.java
@@ -8,7 +8,9 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,16 +21,19 @@ import android.widget.ImageView;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class SelectFormTemplateActivity extends AppCompatActivity {
     public final static String TEMP_REF = "com.wallace.happy.androidformwiz.TEMP_REF";
 
-    private String TAG ="SELECT FORM ________";
+    private static final String TAG = "SELECT_FORM";
     private int PICK_IMAGE_REQUEST = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_form_template);
+        Log.v(TAG, "HELLO");
     }
 
 
@@ -54,54 +59,94 @@ public class SelectFormTemplateActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
 
-            final Uri uri = data.getData();
-            Bitmap bitmap = null;
-            try {
-                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
-                boolean bo = bitmap==null;
-                Log.v(TAG, "HERE" );
+            Uri uri = data.getData();
+            try{
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageBitmap(bitmap);
+        }
+        else if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Log.v(TAG, "HELLO");//mCurrentPhotoPath);
+            File f = new File(mCurrentPhotoPath);
+            Uri contentUri = Uri.fromFile(f);
+            Log.v(TAG, photoURI.toString());
+            Log.v(TAG, contentUri.toString());
+            try{
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), contentUri);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageBitmap(bitmap);
+            /*Bundle extras = data.getExtras();
+            bitmap = (Bitmap) extras.get("data");
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageBitmap(bitmap);
+            try{
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
-            /*// Let's read picked image data - its URI
-            Uri pickedImage = data.getData();
-            // Let's read picked image path using content resolver
-            String[] filePath = { MediaStore.Images.Media.DATA };
-            Cursor cursor = getContentResolver().query(pickedImage, filePath, null, null, null);
-            cursor.moveToFirst();
-            String imagePath = cursor.getString(cursor.getColumnIndex(filePath[0]));
-            cursor.close();
-
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-            bitmap = BitmapFactory.decodeFile(imagePath, options);*/
-
-            ImageView imageView = (ImageView) findViewById(R.id.imageView);
-            imageView.setImageBitmap(bitmap);
-
-        }
-        else if (requestCode == REQUEST_IMAGE_CAPTURE && data != null && data.getExtras() != null){
-            Bundle extras = data.getExtras();
-            bitmap = (Bitmap) extras.get("data");
-            ImageView imageView = (ImageView) findViewById(R.id.imageView);
-            imageView.setImageBitmap(bitmap);
+            ImageView img = (ImageView) findViewById(R.id.imageView);
+            img.setImageURI(imageUri);*/
         }
     }
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
+    static final int REQUEST_TAKE_PHOTO = 1;
+    Uri photoURI;
     /**
      *  Called when the user clicks the camera button
      * Opens Camera so they can capture an image
      * */
     public void openCamera(View view) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                photoURI = FileProvider.getUriForFile(this,
+                        "com.wallace.happy.androidformwiz.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
         }
     }
 
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
 
     public void processImageScreen(View view) {
         if(bitmap!=null) {
